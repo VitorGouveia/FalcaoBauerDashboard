b5f3664cac6f708f827bf8daa4a2bb12dc8fde45
vitorgouveia <vitor.gouveia@aluno.etecjrm.com.br>
2021-09-01 20:03:55 -0300

    fix: add new components css into app

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,26 @@ import '../styles/home.css'
 import '../styles/global.css'
 import '../styles/login.css'
 import '../styles/register.css'
+import '../styles/input.css'
+import '../styles/button.css'
 
-const App = ({ Component, pageProps }) => <Component {...pageProps} />
+import Head from "next/head"
+
+const App = ({ Component, pageProps }) => {
+  return (
+    <>
+      <Head>
+        <title>ETEC JRM</title>
+        <meta charSet="UTF-8"/>
+        <link rel="icon" href="/etec.svg" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+        <meta name="author" content="vitor neves gomes gouveia" />
+      </Head>
+      <main>
+        <Component {...pageProps} />
+      </main>
+    </>
+  )
+}
 
 export default App
